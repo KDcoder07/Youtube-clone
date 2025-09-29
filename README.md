06c3709efe888fda8fc16fb4ec606eb7c0f9e259
KDcoder07 <kshitijdeokar02@gmail.com>
2025-09-29 15:35:00 +0530

    Initial commit - YouTube Clone project

---
+++ README.md
@@ -0,0 +1,8 @@
+React-Based YouTube Clone
+
+Discover a new era of video-sharing with our React-based YouTube clone. This project redefines the user experience with its intuitive interface, responsive design, and innovative features. From dynamic recommendations and real-time interactivity to advanced search capabilities, our clone is more than just a mimicry of popular platforms. Content creators benefit from easy video management, while viewers enjoy a seamless and personalized browsing experience. Powered by React on the frontend, a robust server on the backend, and secure authentication, this clone is designed to elevate your video-sharing journey. Join us and explore a world where sharing and discovering content is effortless and enjoyable.
+<br>
+View Project Here :-https://pineartistry-youtube-clone.netlify.app/
+
+<img src="https://github.com/Anurag22060/youtube_clone/blob/master/src/Images/Youtube_clone.png?raw=true" alt="youtube" />
+
---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,46 @@
+import { Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import {Videos,ChannelCard} from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
+
+const ChannelDetail = () => {
+const [channelDetail, setChannelDetail] = useState(null);
+const [videos, setVideos] = useState([])
+const {id} =useParams();
+
+useEffect(() => {
+ fetchFromAPI(`channels?part=snippet&id=${id}`)
+ .then((data) =>setChannelDetail(data?.items[0]));
+
+ fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+ .then((data) =>setVideos(data?.items));
+
+}, [id])
+
+  
+  return (
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{background: 'linear-gradient(90deg, rgba(5,252,249,1) 0%, rgba(246,8,213,1) 100%)',
+        zIndex: 10,
+        height:'300px',
+        
+        }} />
+
+       <ChannelCard channelDetail={channelDetail}
+          marginTop="-110px"
+       />
+      </Box>
+      <Box display="flex" p={2}>
+
+        <Box sx={{mr:{sm:'100px'}}}/>
+            <Videos videos={videos} />
+        
+      </Box>
+    </Box>
+  )
+}
+
+export default ChannelDetail
